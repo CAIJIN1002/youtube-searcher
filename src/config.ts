2553c4e3891451410fc5fd875cb4fd92b752cecc
John <cba46532@gmail.com>
2020-11-23 09:57:13 +0800

    rm API_KEY

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { ConfigType } from 'types/config';
 
 const config: ConfigType = {
   youtube: {
-    API_KEY: 'AIzaSyA1y4zTm-QJsKwQ2oxCD3_-3hpXEptWgHY',
+    API_KEY: 'API_KEY',
     urls: {
       getSearchUrl(this: ConfigType, query: string) {
         return `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&type=video&key=${this.youtube.API_KEY}&q=${query}`;
